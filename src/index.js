2fb53917e73c7670b34e939cd38c2c1ff17ee063
Adam Mandeville <adam.mandeville@vitalsource.com>
2021-02-18 10:12:21 -0500

    working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Keeper from './Keeper';
+// import Keeper from './Keeper';
 import reportWebVitals from './reportWebVitals';
+import Enrichments, { Component } from "mosaic-components";
+import { createTheme } from "@vitalsource/vstui/themes";
+import { brick } from "@vitalsource/vstui/colors";
+
+// Enrichments.getManifest(
+//   "900c2fc0-043c-4910-9253-bec7b024efce"
+//  ).then(console.log);
+const myTheme = createTheme({ color: { primary: brick } });
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Keeper />
+    <Component theme={myTheme} vaultId="207046"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
